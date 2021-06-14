e488be0f9ec831da63cf4e9d47f492c0d1f6fc8a
NajeebUllah161 <najeebkhannk123@yahoo.com>
2021-06-14 19:44:18 +0500

    ch(app): Add application Files

--- employeeapi/models.py
+++ employeeapi/models.py
@@ -6,8 +6,6 @@ class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
     mobile = models.CharField(max_length=15)
-    email = models.EmailField(max_length=50, default="", editable=False)
-    
 
 
 #CRUD
